83cf106780bcafd12444b01ed35b3b3dafe8b5ac
Sanarielsen <sanarielsen.dev@gmail.com>
2025-08-20 17:56:20 -0300

    feat(PSW-02-03): create orders, filters and first part of charts
    Create the comports will be show the data relationed to orders, filters
    and the first part of the charts in the dashboard.
    
    We used the components originally created on shadcn UI library.

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import { Dashboard } from './pages/app/dashboard'
+import { Dashboard } from './pages/app/dashboard/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { SignUp } from './pages/auth/sign-up'
+import { Orders } from './pages/app/orders/orders'
 
 export const router = createBrowserRouter([
 
@@ -12,7 +13,8 @@ export const router = createBrowserRouter([
     path: '/',
     element: <AppLayout />,
     children: [
-      {path: "/", element: <Dashboard /> }
+      {path: "/", element: <Dashboard /> },
+      {path: "/orders", element: <Orders /> }
     ]
   },
     {
