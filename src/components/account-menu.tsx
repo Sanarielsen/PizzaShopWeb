48243423f1b4280c59947d2cdf35642649be1e40
Sanarielsen <sanarielsen.dev@gmail.com>
2025-08-25 11:58:17 -0300

    feat(PSW-03-02): add positive interface behavior
    Add positive interface because sometimes like a 98% of executions are
    be successful and we want update that previous of the route can be
    executed, but we need add something if the route fails.

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,14 +1,17 @@
 import { Building, ChevronDown, LogOut } from "lucide-react";
 import { Button } from "./ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 import { getProfile } from "@/api/get-profile";
 import { Skeleton } from "./ui/skeleton";
 import { Dialog, DialogTrigger } from "./ui/dialog";
 import { StoreProfileDialog } from "./store-profile-dialog";
+import { useNavigate } from "react-router-dom";
+import { signOut } from "@/api/sign-out";
 
 export function AccountMenu() {
+  const navigate = useNavigate();
 
   const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ['profile'],
@@ -20,6 +23,13 @@ export function AccountMenu() {
     queryFn: getManagedRestaurant,
     staleTime: Infinity,
   })
+
+  const { mutateAsync: signOutFn, isPending: isSigninOut} = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navigate('/sign-in', {replace: true})
+    }
+  }) 
 
   return (
     <Dialog>
@@ -54,9 +64,11 @@ export function AccountMenu() {
               <span>Perfil da loja</span>
             </DropdownMenuItem>
           </DialogTrigger>
-          <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-            <LogOut className="mr-2 h-4 w-4 text-rose-500 dark:text-rose-400" />
-            <span>Sair</span>
+          <DropdownMenuItem asChild className="text-rose-500 dark:text-rose-400" disabled={isSigninOut}>
+            <button className="w-full" onClick={() => signOutFn()}>
+              <LogOut className="mr-2 h-4 w-4 text-rose-500 dark:text-rose-400" />
+              <span>Sair</span>
+            </button>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
